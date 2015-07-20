7a443873d2e4eb59b3d4cc4803fa103b59bf31fb
MVARGAS <MVARGAS@MVARGAS.local>
2015-07-20 10:55:42 -0500

    Initial Commit

---
+++ ProyectoFinal/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ProyectoFinal
+//
+//  Created by MVARGAS on 20/07/15.
+//  Copyright (c) 2015 MVARGAS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ProyectoFinal/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ProyectoFinal
+//
+//  Created by MVARGAS on 20/07/15.
+//  Copyright (c) 2015 MVARGAS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
